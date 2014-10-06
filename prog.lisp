fa4782486c34591610d6b4450394015f96e8a816
nylesb <nbreecher@gmail.com>
2014-10-05 20:53:31 -0500

    Improved test structure; green: function to access maze position

--- prog.lisp
+++ prog.lisp
@@ -1,7 +1,11 @@
 ;;; Helper functions
 
-(defun process-input ()
-  :eof)
+;;; Returns the value at position (x,y) in maze
+(defun pos (maze x y) ; maze a list
+  (nth y (nth x maze)))
+
+(defun navigate (maze startx starty) ; maze a list
+  (pos maze startx starty))
 
 ;;; Main function
 
--- unittests/tests.lisp
+++ unittests/tests.lisp
@@ -1,25 +1,36 @@
 ;;; Contains unit tests for the maze solver program, prog.lisp.
 ;;; This file runs all tests and displays results
 
-;;; Setup
+;;; Preparation
 
 (cond ((ignore-errors (load "prog.lisp")) nil) ; Run from maze-solver dir
       ((ignore-errors (load "../prog.lisp")) nil) ; Run from unittests dir
       (t (print "Failure to load program file.")))
-(terpri) ; Readability
-(terpri) ; Readability
 
 ;;; Catches errors testcase might throw, then prints fail/pass to output.
 (defun run-test (testcase) ; testcase is a function
-  (if (eval (ignore-errors (funcall testcase)))
-    (princ (format nil "   O Pass: ~A ~%" (documentation testcase 'function)))
-    (princ (format nil " X   Fail: ~A ~%" (documentation testcase 'function)))))
+  (if (ignore-errors (funcall testcase))
+      (princ (format nil "   O Pass: ~A ~%" (documentation testcase 'function)))
+      (princ (format nil " X   Fail: ~A ~%" (documentation testcase 'function)))))
 
+;;; Throws an error if unequal, allowing run-test to catch test failures.
+;;; Also prints to display an error message.
+(defun assert-equal (expected actual)
+  (if (equal expected actual)
+      (eval t)
+      (error (princ (format nil "Wanted ~A, got ~A." expected actual)))))
+  
 ;;; Test cases
 
-(defun should-read-input-file ()
-  "Should read input file until end of file."
-  (equal (process-input) :eof))
-(run-test #'should-read-input-file)
+(terpri) ; Readability
+
+(run-test (lambda ()
+  "Should have function to access position in maze."
+  (let ((maze '((t O O)
+                (+ + "cat"))))
+    (assert-equal t (pos maze 0 0))
+    (assert-equal "cat" (pos maze 1 2))
+    (assert-equal 'O (pos maze 0 1))
+    (assert-equal nil (pos maze 10 10)))))
 
 (terpri) ; Readability

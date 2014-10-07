cf2786a6363a3275f10558be3e03ba001ba81264
nylesb <nbreecher@gmail.com>
2014-10-07 17:17:54 -0500

    Green tracks amount of moves made.

--- prog.lisp
+++ prog.lisp
@@ -8,32 +8,37 @@
         (row (second maze-list))
         (col (third maze-list))
         (pos-type nil)
+        (moves 0)
         (path '(START))) ; path stores movements in reverse order
     (labels ((pos (i j)
                (if (and (> i -1) (> j -1))
                    (nth j (nth i maze))
                    nil))
+             (results (message)
+               (list (reverse path) (print message) maze moves))
              (navigate ()
                (setf pos-type (pos row col))
                ;; Stop navigating upon finding an exit or hitting an obstacle
                (cond ((or (equal pos-type '+) (equal pos-type nil) (equal pos-type 'X))
-                      (return-from navigate (list (reverse path) (print "Invalid location."))))
+                      (progn (setf moves (- moves 2))
+                             (return-from navigate (results "Invalid location."))))
                      ((equal pos-type 'E)
-                      (return-from solve-maze (list (reverse path) (print "Hooray! I am free.") maze))))
+                      (return-from solve-maze (results "Hooray! I am free."))))
                ;; Try every direction from current location
                (setf (nth col (nth row maze)) 'X) ; Mark current location
-               (setf col (+ col 1) path (append '(R) path)) ; Right
+               (setf col (+ col 1) path (append '(R) path) moves (+ moves 1)) ; Right
                (navigate)
-               (setf col (- col 1) path (cdr path)) ; Backtrack
-               (setf row (+ row 1) path (append '(D) path)) ; Down
+               (setf col (- col 1) path (cdr path) moves (+ moves 1)) ; Backtrack
+               (setf row (+ row 1) path (append '(D) path) moves (+ moves 1)) ; Down
                (navigate)
-               (setf row (- row 1) path (cdr path)) ; Backtrack
-               (setf col (- col 1) path (append '(L) path)) ; Left
+               (setf row (- row 1) path (cdr path) moves (+ moves 1)) ; Backtrack
+               (setf col (- col 1) path (append '(L) path) moves (+ moves 1)) ; Left
                (navigate)
-               (setf col (+ col 1) path (cdr path)) ; Backtrack
-               (setf row (- row 1) path (append '(U) path)) ; Up
+               (setf col (+ col 1) path (cdr path) moves (+ moves 1)) ; Backtrack
+               (setf row (- row 1) path (append '(U) path) moves (+ moves 1)) ; Up
                (navigate)
-               (setf row (+ row 1) path (cdr path)) ; Backtrack
+               (setf row (+ row 1) path (cdr path) moves (+ moves 1)) ; Backtrack
                (setf (nth col (nth row maze)) 'O) ; Unmark
-               (return-from navigate (list path "Help! I am trapped.")))) ; Paths exhausted
+               (return-from navigate (results "Help! I am trapped.")))) ; Paths exhausted
       (navigate))))
+      ;(list (reverse path) (print message) maze moves))))
--- unittests/tests.lisp
+++ unittests/tests.lisp
@@ -73,12 +73,12 @@
   "Should mark path on maze and not cross it."
   (let ((maze     '((O O + + +)
                     (+ O O O +)
-                    (+ O O E +)))
+                    (+ E O O +)))
         (expected '((X X + + +)
                     (+ X X X +)
-                    (+ O O E +))))
+                    (+ E X X +))))
     (assert-equal expected (third (solve-maze :maze-list (list maze 0 0))))
-    (setf maze '((O O O +)))
+    (setf maze '((O O O +))) ; Checks not crossing, otherwise infinite recursion
     (assert-equal failure-message (second (solve-maze :maze-list (list maze 0 0)))))))
 
 (run-test (lambda ()
@@ -99,10 +99,35 @@
   "Should navigate a maze with simple backtracking."
   (let ((maze '((+ + + + + + +)
                 (+ E O O O O +)
-                (+ + + + + + +)))
+                (+ + + + O + +)))
         (expected '(START L L)))
     (assert-equal expected (first (solve-maze :maze-list (list maze 1 3))))
     (setf (nth 1 (nth 1 maze)) 'O) ; No exit now
     (assert-equal failure-message (second (solve-maze :maze-list (list maze 1 3)))))))
+
+(run-test (lambda ()
+  "Should navigate complicated maze which requires backtracking."
+  (let ((maze '((+ O O O O O +)
+                (+ O + + + O O)
+                (O O O O + + O)
+                (O + O O O + O)
+                (O + + + + + O)
+                (O O O + O + O)
+                (O + O O O + O)
+                (E + + + + + +)))
+        (expected '(START U L L L L D D L D D D D D)))
+    (assert-equal expected (first (solve-maze :maze-list (list maze 1 5))))
+    (setf (nth 0 (nth 7 maze)) 'O) ; No exit
+    (assert-equal failure-message (second (solve-maze :maze-list (list maze 1 5)))))))
+
+(run-test (lambda ()
+  "Should return number of moves made (including backtracking)."
+  (let ((maze '((+ + + + + + +)
+                (+ E O O O O +)
+                (+ + + + O + +)))
+        (expected 9))
+    (assert-equal expected (fourth (solve-maze :maze-list (list maze 1 2))))
+    (setf (nth 1 (nth 1 maze)) 'O expected (+ expected 1)) ; No exit now
+    (assert-equal expected (fourth (solve-maze :maze-list (list maze 1 2)))))))
 
 (terpri) ; Readability
